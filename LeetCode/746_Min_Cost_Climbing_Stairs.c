c4934e7617b5b631a4c3aaeb6fe79fd898e1c691
EduardoSPorto <edaurdosamporto@gmail.com>
2023-07-03 09:49:04 -0300

    Resposta funcional Anteriormente, como previsto,  a resposta não funciona, esta funciona perfeitamente

--- LeetCode/746_Min_Cost_Climbing_Stairs.c
+++ LeetCode/746_Min_Cost_Climbing_Stairs.c
@@ -8,16 +8,17 @@ int main()
 {
     int *costMain;
     costMain = (int*) malloc(sizeof(int) * 10);
-    costMain[0] = 10;
-    costMain[1] = 15;
-    costMain[2] = 20;
-    costMain[3] = 20;
-    costMain[4] = 20;
-    costMain[5] = 20;
-    costMain[6] = 20;
-    costMain[7] = 20;
-    costMain[8] = 20;
-    costMain[9] = 20;
+    costMain[0] = 1;
+    costMain[1] = 100;
+    costMain[2] = 1;
+    costMain[3] = 1;
+    costMain[4] = 1;
+    costMain[5] = 100;
+    costMain[6] = 1;
+    costMain[7] = 1;
+    costMain[8] = 100;
+    costMain[9] = 1;
+    //1,100,1,1,1,100,1,1,100,1
     /*
         int c = 0;
     while(c <= 9){
@@ -25,9 +26,10 @@ int main()
         *costMain++;
         c++;
     }
+    
+    //printf("\n\n\n%d", 10);
     */
-    printf("\n\n\n%d", 10);
-
+   
     printf("%d ",minCostClimbingStairs(costMain, 10));
     //free(costMain);
     return 0;
@@ -36,31 +38,20 @@ int main()
 
  int minCostClimbingStairs(int* cost, int costSize){
 
-    int resp,i;
-    int *newCost = cost;
+    int resp ,i , *newCost = cost;
 
     newCost = (int*) realloc(newCost, sizeof(int ) * (costSize + 1));
 
-    int c=0;
-    newCost[costSize] = 0;
-
-        while(c <= 9){
-        printf("\n%d",*newCost);
-        *newCost++;
-        c++;
-    }
-/*
     for(i = costSize - 3; i >= 0; i--){
-        if (cost[i + 1] < cost[i + 2])
+        if (newCost[i + 1] < newCost[i + 2])
         {
-            cost[i] = cost[i] + cost[i + 1];
+            newCost[i] = newCost[i] + newCost[i + 1];
         }
         else
-        cost[i] = cost[i] + cost[i + 2];
+        newCost[i] = newCost[i] + newCost[i + 2];
     }
-    if (cost[0] < cost [1])
-        return cost[0];
-        //else
-        return cost[1];
-*/
+    if (newCost[0] < newCost [1])
+        return newCost[0];
+        return newCost[1];
+
 }
